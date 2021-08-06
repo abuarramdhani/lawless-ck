bcfda0d0425bf0e9f05861245415509d099fa638
javaxcode <58064721+javaxcode@users.noreply.github.com>
2021-08-06 19:10:27 +0700

    f6

--- controller/c_kaskecil.php
+++ controller/c_kaskecil.php
@@ -1,61 +1,22 @@
 <?php
-$saldo_kas = query("SELECT saldo FROM kas WHERE id=1")[0];
-$saldo_mutasi = $saldo_kas['saldo'];
-$kaskecil = query("SELECT * FROM kas ORDER BY id DESC");
+$kaskecil = query("SELECT * FROM produk ORDER BY id DESC");
 
-$kodeakune = query("SELECT * FROM kodeakun WHERE kodeakun2='52' OR kodeakun2='51' AND kodeakun3!='526' ORDER BY ketkode3 ASC");
-$kodeakunp = query("SELECT * FROM kodeakun WHERE kodeakun3='433' OR kodeakun3='434' ORDER BY id ASC");
 
-$bulansebelumnya = date('m', strtotime('-1 month', strtotime(date('Y-m-d'))));
-if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM kas ")) === 1) {
+//$totalsupplier = mysqli_num_rows(query("SELECT COUNT(id) FROM supplier "))[0];
+// $totalbahan = query("SELECT COUNT(*) FROM bahan ")[0];
+// $totalproduk = query("SELECT COUNT(*) FROM produk ")[0];
 
-    $kass = query("SELECT * FROM kas WHERE month(tanggal) ='03' ORDER BY id DESC LIMIT 1")[0];
-    $saldokass = $kass["saldo"];
-} else {
-    $saldokass = 0;
-}
+$sql="SELECT COUNT(id) FROM supplier";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_array($result);
+$totalsupplier = $row[0];
 
-if (isset($_POST["tampilkan"])) {
-    $bulan = $_POST["bulan"];
-    $tahun = $_POST["tahun"];
+$sql="SELECT COUNT(id) FROM bahan";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_array($result);
+$totalbahan = $row[0];
 
-    if ($bulan === "Bulan") {
-        header("location:index");
-    }
-
-    $kary = query("SELECT * FROM karyawan ORDER BY id ASC");
-    $kaskecil = query("SELECT * FROM kas WHERE month(tanggal) ='$bulan' AND year(tanggal) ='$tahun' ORDER BY id DESC");
-    $kas = query("SELECT * FROM kas WHERE month(tanggal) ='$bulan' AND year(tanggal) ='$tahun' ORDER BY id DESC LIMIT 1")[0];
-    $saldokas = $kas["saldo"];    
-
-    $jumlahi = "SELECT SUM(input) AS total_i FROM kas WHERE month(tanggal) ='$bulan' AND year(tanggal) ='$tahun'"; //perintah untuk menjumlahkan
-    $hasili = mysqli_query($conn, $jumlahi); //melakukan query dengan varibel $jumlahkan
-    $inp = mysqli_fetch_array($hasili); //menyimpan hasil query ke variabel $t
-    $totali = $inp['total_i'];
-
-    $jumlaho = "SELECT SUM(output) AS total_o FROM kas WHERE month(tanggal) ='$bulan' AND year(tanggal) ='$tahun'"; //perintah untuk menjumlahkan
-    $hasilo = mysqli_query($conn, $jumlaho); //melakukan query dengan varibel $jumlahkan
-    $out = mysqli_fetch_array($hasilo); //menyimpan hasil query ke variabel $t
-    $totalo = $out['total_o'];
-
-    if ($bulan != $month) {
-        $jumlahsaldo = $totali - $totalo;
-    } else {
-        
-        $kas = query("SELECT * FROM kas WHERE month(tanggal) ='$bulansebelumnya' ORDER BY id DESC LIMIT 1")[0];
-        $saldokas = $kas["saldo"];
-        $jumlahsaldo = $saldokas + ($totali - $totalo);
-    }
-} else {
-    $jumlahi = "SELECT SUM(input) AS total_i FROM kas WHERE month(tanggal) ='$month' AND year(tanggal) ='$year'"; //perintah untuk menjumlahkan
-    $hasili = mysqli_query($conn, $jumlahi); //melakukan query dengan varibel $jumlahkan
-    $inp = mysqli_fetch_array($hasili); //menyimpan hasil query ke variabel $t
-    $totali = $saldokass + $inp['total_i'];
-
-    $jumlaho = "SELECT SUM(output) AS total_o FROM kas WHERE month(tanggal) ='$month' AND year(tanggal) ='$year'"; //perintah untuk menjumlahkan
-    $hasilo = mysqli_query($conn, $jumlaho); //melakukan query dengan varibel $jumlahkan
-    $out = mysqli_fetch_array($hasilo); //menyimpan hasil query ke variabel $t
-    $totalo = $out['total_o'];
-
-    $jumlahsaldo = $totali - $totalo;
-}
+$sql="SELECT COUNT(id) FROM produk";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_array($result);
+$totalproduk = $row[0];
